d2c5110fc3219140809ef45e78c2e8def48fb946
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-08-27 20:55:22 +0530

    done

--- mini_bank_app.py
+++ mini_bank_app.py
@@ -5,7 +5,7 @@ class customer:
 
     def deposit(self,amount):
         self.balance=self.balance+amount
-        print(self.balance)
+        print("Your current balance",self.balance)
 
     def withdraw(self,amount):
         if amount<=self.balance:
@@ -16,3 +16,27 @@ class customer:
             print("insufficient Balance")
 
 print("welcome to the AAPKAbank")
+name=input("enter your name: ")
+c=customer(name)
+while True:
+    print()
+    print("Enter d for Deposit : ")
+    print("Enter w for Withdraw : ")
+    print("Enter e for Exit :")
+    option=input("what do you want from the options : ")
+
+    if option.lower()=='d':
+        amount=int(input("enter amount you want to deposit"))
+        c.deposit(amount)
+    elif option.lower()=='w':
+        amount=int(input("enter amount you want to withdraw"))
+        c.withdraw(amount)
+    elif option.lower()=='e':
+        break
+    else:
+        print("Please Try Again!")
+    
+
+
+    
+    
